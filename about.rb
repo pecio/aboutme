db4e0a61d1212ed86c3224d544a140dbf76467d1
Raúl Pedroche <r.pedroche.novillo@gmail.com>
2014-04-29 13:06:15 +0200

    More configuration for i18n gem

--- about.rb
+++ about.rb
@@ -1,8 +1,20 @@
 require 'sinatra'
 require 'i18n'
+require 'i18n/backend/fallbacks'
 require 'rack/contrib'
 
+configure do
+  I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
+  I18n.load_path = Dir[File.join(settings.root, 'locales', '*.yml')]
+  I18n.backend.load_translations
+end
+
 use Rack::Locale
+
+before '/:locale/*' do
+    I18n.locale       =       params[:locale]
+      request.path_info = '/' + params[:splat ][0]
+end
 
 get '/' do
   File.new('public/index.html').readlines
