fa089d84cc42f1bb9ebc543165a29043915b16bf
Raúl Pedroche <r.pedroche.novillo@gmail.com>
2014-04-30 00:12:26 +0200

    Setup mail send (untested)

--- about.rb
+++ about.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'i18n'
 require 'i18n/backend/fallbacks'
 require 'rack/contrib'
+require 'pony'
 
 configure do
   I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
@@ -46,4 +47,25 @@ end
 
 get '/contactform.js' do
   erb 'contactform.js'.to_sym, layout: false
+end
+
+post '/contact' do
+  options = {
+    to: 'pedroche@me.com',
+    from: "#{params[:name]} <#{params[:email]}>",
+    subject: 'Contacto de ' + params[:name],
+    body: params[:message],
+    via: :smtp,
+    via_options: {
+      port:           ENV['MAILGUN_SMTP_PORT'],
+      address:        ENV['MAILGUN_SMTP_SERVER'],
+      user_name:      ENV['MAILGUN_SMTP_LOGIN'],
+      password:       ENV['MAILGUN_SMTP_PASSWORD'],
+      authentication: :plain
+    }
+  }
+
+  Pony.mail(options)
+
+  erb :contact
 end
