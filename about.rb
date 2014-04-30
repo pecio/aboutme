9f3acd63550001fd839fa3bf9730f022d135f7e1
Raúl Pedroche <r.pedroche.novillo@gmail.com>
2014-04-30 16:24:53 +0200

    Really initialize I18n in correct order

--- about.rb
+++ about.rb
@@ -5,10 +5,10 @@ require 'rack/contrib'
 require 'pony'
 
 configure do
-  I18n.enforce_available_locales = true
-  I18n.default_locale = :en
   I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
   I18n.load_path = Dir[File.join(settings.root, 'locales', '*.yml')]
+  I18n.enforce_available_locales = true
+  I18n.default_locale = :en
   I18n.backend.load_translations
 end
 
