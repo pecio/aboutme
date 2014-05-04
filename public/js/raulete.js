35ac109cd512e816b0cda649d7fb7e336a01e36c
Raúl Pedroche <r.pedroche.novillo@gmail.com>
2014-05-04 23:17:40 +0200

    Change Resume link to # on mobile
    You were unable to access extended resume on mobile, as submenu was
    never shown.

--- public/js/raulete.js
+++ public/js/raulete.js
@@ -2,3 +2,9 @@ $(window).bind('load', function () {
   $('.starter-template')
     .animate({ opacity: 1 }, { duration: 1000 });
 });
+
+$(document).ready(function () {
+  if ($(window).width() < 768) {
+    $('a.dropdown-toggle').attr('href', '#');
+  }
+});
