d5f8c9dfaafd16fe7eb995853edca18a2e8e47c9
Raúl Pedroche <r.pedroche.novillo@gmail.com>
2014-05-05 20:45:51 +0200

    No longer change resume link to '#' on mobile
    It is now '#' always

--- public/js/raulete.js
+++ public/js/raulete.js
@@ -2,9 +2,3 @@ $(window).bind('load', function () {
   $('.starter-template')
     .animate({ opacity: 1 }, { duration: 1000 });
 });
-
-$(document).ready(function () {
-  if ($(window).width() < 768) {
-    $('a.dropdown-toggle').attr('href', '#');
-  }
-});
