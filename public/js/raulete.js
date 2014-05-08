62837840459ccd5b865776a23752f8263ca205e0
Raúl Pedroche <r.pedroche.novillo@gmail.com>
2014-05-08 16:34:05 +0200

    Show cookies alert only once.

--- public/js/raulete.js
+++ public/js/raulete.js
@@ -2,3 +2,10 @@ $(window).bind('load', function () {
   $('.starter-template')
     .animate({ opacity: 1 }, { duration: 1000 });
 });
+
+$(document).ready(function () {
+  if (document.cookie.replace(/(?:(?:^|.*;\s*)cookiesAlertShown\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true") {
+    $('#alert-cookies').show();
+  document.cookie = "cookiesAlertShown=true; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+  }
+});
