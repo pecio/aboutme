f4e228ad78ee192d6c5211d3d08ceadbd968a6a3
Raul Pedroche Novillo <r.pedroche.novillo@gmail.com>
2020-11-08 00:42:30 +0100

    Fix secrets path

--- secrets.rb
+++ secrets.rb
@@ -2,7 +2,7 @@ require 'google/cloud/secret_manager'
 
 def get_secret(name)
     project = ENV['GOOGLE_CLOUD_PROJECT']
-    path = "/projects/#{project}/secrets/#{name}/versions/latest"
+    path = "projects/#{project}/secrets/#{name}/versions/latest"
     begin
         client = Google::Cloud::SecretManager.secret_manager_service
         response = client.access_secret_version name: path    
