c54209e88b14952456c471ee1e41a2030cd843e8
Raul Pedroche Novillo <r.pedroche.novillo@gmail.com>
2020-11-12 09:38:45 +0100

    Do not try gcp secret manager if there is no envar

--- secrets.rb
+++ secrets.rb
@@ -1,17 +1,25 @@
 require 'google/cloud/secret_manager'
 
 def get_secret(name)
-    project = ENV['GOOGLE_CLOUD_PROJECT']
-    path = "projects/#{project}/secrets/#{name}/versions/latest"
-    begin
-        client = Google::Cloud::SecretManager.secret_manager_service
-        response = client.access_secret_version name: path  
-        response.payload.data  
-    rescue => exception
+    def fallback(name)
         if File.exist?("/run/secrets/#{name}")
             File.read("/run/secrets/#{name}")
         else
             ENV[name.upcase]
         end
+    end
+
+    project = ENV['GOOGLE_CLOUD_PROJECT']
+    path = "projects/#{project}/secrets/#{name}/versions/latest"
+    if project
+        begin
+            client = Google::Cloud::SecretManager.secret_manager_service
+            response = client.access_secret_version name: path  
+            response.payload.data  
+        rescue => exception
+            fallback(name)
+        end
+    else
+        fallback(name)
     end
 end
