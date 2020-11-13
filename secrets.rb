babffd85a14c543a70524b835cb3dbf5651ccd72
Raul Pedroche Novillo <r.pedroche.novillo@gmail.com>
2020-11-13 07:35:20 +0100

    Preload secrets on first page load

--- secrets.rb
+++ secrets.rb
@@ -1,25 +1,66 @@
 require 'google/cloud/secret_manager' if ENV['GOOGLE_CLOUD_PROJECT']
 
-def get_secret(name)
-    def fallback(name)
-        if File.exist?("/run/secrets/#{name}")
-            File.read("/run/secrets/#{name}").chomp
+class SecretStore
+    include Singleton
+
+    def initialize
+        @secrets = {}
+
+        if ENV['GOOGLE_CLOUD_PROJECT']
+            begin
+                @client = Google::Cloud::SecretManager.secret_manager_service
+            rescue
+                @client = nil
+            end
         else
-            ENV[name.upcase]
+            @client = nil
+        end
+
+        for name in [
+            'google_analytics_id',
+            'contact_address',
+            'smtp_login',
+            'smtp_password',
+            'smtp_server',
+            'smtp_port'
+        ]
+            @secrets[name] = _get(name)
         end
     end
 
-    project = ENV['GOOGLE_CLOUD_PROJECT']
-    path = "projects/#{project}/secrets/#{name}/versions/latest"
-    if project
-        begin
-            client = Google::Cloud::SecretManager.secret_manager_service
-            response = client.access_secret_version name: path  
-            response.payload.data  
-        rescue
+    def get(name)
+        if @secrets[name]
+            @secrets[name]
+        else
+            @secrets[name] = _get(name)
+        end
+    end
+
+    private
+    def _get(name)
+        def fallback(name)
+            if File.exist?("/run/secrets/#{name}")
+                File.read("/run/secrets/#{name}").chomp
+            else
+                ENV[name.upcase]
+            end
+        end
+    
+        project = ENV['GOOGLE_CLOUD_PROJECT']
+        path = "projects/#{project}/secrets/#{name}/versions/latest"
+        if @client
+            begin
+                response = @client.access_secret_version name: path  
+                response.payload.data  
+            rescue
+                fallback(name)
+            end
+        else
             fallback(name)
         end
-    else
-        fallback(name)
     end
+end
+
+def get_secret(name)
+    SecretStore.instance.get(name)
 end
